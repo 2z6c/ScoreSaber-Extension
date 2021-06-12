36477d3d6d2f453b6798a38f5c2f57595218c799
nizhack <sgrapher.bear@gmail.com>
2021-06-12 14:32:28 +0900

    fix action buttons in song page

--- src/js/songPage.js
+++ src/js/songPage.js
@@ -6,7 +6,11 @@ import {
 
 window.addEventListener('load',()=>{
   const wrapper = document.querySelector('.column.is-one-quarter');
-  const hash = extractHash( wrapper.firstChild.src );
-  addSongDownloadButton(wrapper,hash);
-  addBookmarkButton(wrapper,hash,location.toString());
+  const hash = extractHash( wrapper.firstElementChild.src );
+  wrapper.insertAdjacentHTML('beforeend',`
+  <div id="song-action-panel"></div>
+  `);
+  const panel = document.getElementById('song-action-panel');
+  addSongDownloadButton(panel,hash);
+  addBookmarkButton(panel,hash,location.toString());
 });
