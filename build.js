d65df6a089a0ae28642814cd00f41b3b251cdaea
nizhack <sgrapher.bear@gmail.com>
2021-06-14 10:32:22 +0900

    improve build and watch

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
-const watch = process.env.WATCH = false;
+const watch = process.env.WATCH = !!process.argv[3];
 import esbuild from 'esbuild';
+import {sassPlugin} from 'esbuild-sass-plugin';
 esbuild.build({
   entryPoints: [
     'src/js/userProfile.js',
@@ -8,15 +9,51 @@ esbuild.build({
     'src/js/popup.js',
     'src/js/leaderboard.js',
     'src/js/rankingRequest.js',
-    'src/background.js'
+    'src/background.js',
   ],
+  
   bundle: true,
+  loader: { '.json': 'text' },
   outdir: 'dist',
   define: {
-    'process.env.NODE_ENV': '"production"'
+    'process.env.NODE_ENV': process.env.NODE_ENV,
   },
-  minify: true,
-  sourcemap: false,
+  minify: process.argv[2] === 'production',
+  sourcemap: process.argv[2] === 'development',
+  target: ['chrome88'],
+  watch,
+}).catch(()=>process.exit(1));
+
+esbuild.build({
+  entryPoints: [
+    'src/scss/popup.scss',
+    'src/scss/header.scss',
+    'src/scss/leaderboard.scss',
+    'src/scss/songPage.scss',
+    'src/scss/userProfile.scss',
+  ],
+  bundle: true,
+  plugins: [sassPlugin({
+    async transform(css, resolveDir) {
+      const {outputFiles:[out]} = await esbuild.build({
+        stdin: {
+          contents: css,
+          resolveDir,
+          loader: "css",
+        },
+        bundle: true,
+        format: "esm",
+        write: false,
+      });
+      return out.text;
+    }
+  })],
+  outdir: 'dist/css',
+  define: {
+    'process.env.NODE_ENV': process.env.NODE_ENV,
+  },
+  minify: process.argv[2] === 'production',
+  sourcemap: process.argv[2] === 'development',
   target: ['chrome88'],
   watch,
 }).catch(()=>process.exit(1));
